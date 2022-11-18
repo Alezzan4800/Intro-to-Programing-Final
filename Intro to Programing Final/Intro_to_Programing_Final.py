e39087be0e18cb54c31756a0a5d3de91258d52ea
Alezzan4800 <jhdub24@gmail.com>
2022-11-17 21:29:11 -0600

    Update Intro_to_Programing_Final.py

--- Intro to Programing Final/Intro_to_Programing_Final.py
+++ Intro to Programing Final/Intro_to_Programing_Final.py
@@ -4,8 +4,8 @@ hero_str=0
 hero_char=0
 hero_int=0
 hero_vit=0
-hero_sword_bonus=0
-hero_bow_bonus=0
+hero_sword_bonus=40
+hero_bow_bonus=40
 hero_gold=0
 hero_health=100+hero_vit
 hero_lvl=1
@@ -29,9 +29,13 @@ f"There had been a time of peace for about 2 years while all the nations battled
 input("During dialong or narration please press enter continue.")
 
 ## Character Creation time
+## Naming Character
+
 print("Time to create your Character")
 
-hero_name=input(str("Plesae enter your charcter's name: "))
+hero_name=input(str("Plesae enter your charcter's name: "))\
+
+##Chosing Background
 
 while True:
            background_choice=int(input("Please choose a background\n 1-You grew up as an indentured servant in Gorthomon.\n 2-You grew up a slave to the Orcs.\n 3-You grew up as book keep in Timberwolf. \n 4-You grew up as a prince in Foxborough\n"))
@@ -58,6 +62,8 @@ while True:
            else:
                print("Please chose one of the avalabe choices")
 
+## Choosing Training
+
 while True:
     training_choice=int(input("Please chose a training.\n After leave home you chose to learn the art of:\n 1-Range fighting while traveling with a hunting party. \n 2-Sword fighting while being a squire for some traveling Knights \n 3-Negotiation while traveling with a party of traders. \n"))
     if training_choice==1:
@@ -72,7 +78,9 @@ while True:
     else:
         print("Please chose one of the avalabe choices")
 
-print("Present Day:\n You have been summoned1 before the King of foxborough")
+## Time for the Game to Start
+## Story Time
+print("Present Day:\n You have been summoned before the King of Foxborough")
 input()
 print(
     F"{hero_name}: Hello, King Remzii, you have summoned me today\n"
@@ -80,8 +88,9 @@ print(
     "You will have to travel undercover as it can not be known that we have a peace deal in the works with the Dwarves.\n"
     "we would incur an attack and full-on invasion from the Elfs is this was to become known.\n"
     )
+## First Conversation playing has control in
 while True:
-    conversation_choice=int(input("1-Why would this incur an invasion?\n 2-Why did the Dwarves agree to this deal?\n 3-Ok Sir I will make sure she gets there unharmed and secretly.\n"))
+    conversation_choice=int(input(" 1-Why would this incur an invasion?\n 2-Why did the Dwarves agree to this deal?\n 3-Ok Sir I will make sure she gets there unharmed and secretly.\n"))
     if conversation_choice==1:
         print("Elfs have long for a true reason to invade us.  A human and Dwarf alliance would be just the thing they need.")
     elif conversation_choice==2:
@@ -94,4 +103,50 @@ while True:
 
 print(f"you rest until it is dusk then leave with the Princess.\n As you cross the boarders a group of orcs attack.")
 
-   
+## First Battle
+while True:
+    enemy_battlefield=10
+    print(enemy_battlefield)
+    if enemy_battlefield ==0:
+        break
+
+    battle_discussion=int(input(" 1-Ranged Attack \n 2-Melee Attack \n 3-Discussion \n 4-Defend \n 5-Run \n"))
+    if battle_discussion==1:
+        range_attack=hero_bow_bonus+random.randrange(100)
+        if range_attack >= 60:
+                enemy_battlefield-=1
+        enemy_attack=random.randrange(100)
+        if enemy_attack >= 50:
+            hero_health -=15
+
+    if battle_discussion==2:
+        melee_attack=hero_sword_bonus+random.randrange(100)
+        if melee_attack >= 60:
+                enemy_battlefield-=1
+        enemy_attack=random.randrange(100)
+        if enemy_attack >= 50:
+            hero_health -=15
+
+    if battle_discussion==3:
+        discussion_attack=hero_char+random.randrange(100)
+        if discussion_attack >= 60:
+                enemy_battlefield-=1
+
+        enemy_attack=random.randrange(100)
+        if enemy_attack >= 50:
+            hero_health -=15
+
+    if battle_discussion==4:
+        enemy_attack=random.randrange(100)
+        if enemy_attack >= 75:
+            hero_health -=15
+
+
+    if battle_discussion==5:
+        print("no where to go in this battle")
+
+    if battle_discussion<=1 or battle_discussion>=5:
+        print("This is not an option")
+       
+
+
